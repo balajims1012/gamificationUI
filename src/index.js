380049be7b7f5d1bfeaafab19cf82cfa65eadc9e
Haley Harelson <hharelso@asu.edu>
2020-11-03 12:06:12 -0700

    Added Less capabilities for custom themes and styled the progress bar a little bit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Steps } from "antd";
-import "antd/dist/antd.css";
-import "./index.css";
+import "./index.less";
 
 const { Step } = Steps;
 
 ReactDOM.render(
   <div className="App">
-  <Steps current={1} percent={60}>
-    <Step title="Finished" description="This is a description." />
-    <Step title="In Progress" subTitle="Left 00:00:08" description="This is a description." />
-    <Step title="Waiting" description="This is a description." />
+  <Steps current={1} direction={"horizontal"}>
+    <Step title="Level 1" />
+    <Step title="Level 2" subTitle="Left: 2 Referrals" />
+    <Step title="Level 3" />
+    <Step title="Level 4" />
   </Steps>
   </div>,
   document.getElementById("root")
